ed925355147b681e1cc98744a43c276fce91fd5b
lily-uzh <lily.watanabe@uzh.ch>
2024-11-23 16:44:59 +0100

    cluster run trials

--- build-script.sh
+++ build-script.sh
@@ -8,5 +8,5 @@ module load openmpi
 module load cmake
 
 cmake -B/home/lwatan/data/SPH-EXA-fork/build -S/home/lwatan/data/SPH-EXA-fork -DSPH_EXA_WITH_GRACKLE=OFF
-cd sphexa 
+cd build/sphexa 
 make sphexa
--- run_disk.sh
+++ run_disk.sh
@@ -3,15 +3,21 @@
 #SBATCH --output=disk_comb_1mil.out        # Output file (default: slurm-%j.out)
 #SBATCH --error=disk_comb_1mil.err         # Error file  (default: slurm-%j.out)
 #SBATCH --cpus-per-task=4         # Number of CPUs per task
-#SBATCH --nodes=4
+#SBATCH --ntasks=4                # Number of tasks
+#SBATCH --ntasks-per-node=1      # Number of tasks per node§
 #SBATCH --mem-per-cpu=4G          # Memory per CPU
-#SBATCH --time=02:00:00           # Wall clock time limit
+#SBATCH --time=00:30:00           # Wall clock time limit
 
 module load openmpi
 module load hdf5
 
 OMP_NUM_THREADS=4
 export OMP_NUM_THREADS
+export OMPI_MCA_btl=tcp,self
 
 # ./main/src/sphexa/sphexa --init '/home/lwatan/data/cloud.h5' --prop std-angmom -s 3 -w 1 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o ../output/run_cloud_3_5.hdf5
-mpirun -np 4 build/main/src/sphexa/sphexa --init '/home/lwatan/data/disk5.hdf5' --prop std-angmom -s 1e6 -w 10 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o ../output/run_disk_comb_1mil.hdf5
+# run with 4 MPI processes and 4 OpenMP threads per MPI process
+mpirun -np 4 build/main/src/sphexa/sphexa --init '/home/lwatan/data/disk5.hdf5' --prop std-angmom -s 100 -w 10 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o '/home/lwatan/data/SPH-EXA-fork/output/run_disk_comb_1mil.hdf5'
+
+#srun build/main/src/sphexa/sphexa --init '/home/lwatan/data/disk5.hdf5' --prop std-angmom -s 100 -w 10 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o ../output/run_disk_comb_1mil.hdf5
+
