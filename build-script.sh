07bf74f23e8cfaf8bf2fbaa8f264e4c308d605e0
lily-uzh <lily.watanabe@uzh.ch>
2024-11-25 11:41:56 +0100

    some build stuff

--- build-script.sh
+++ build-script.sh
@@ -3,10 +3,14 @@
 module load gcc/14.2.0
 module load python
 module load gpu
-module load hdf5
 module load openmpi
+module load hdf5
 module load cmake
 
-cmake -B/home/lwatan/data/SPH-EXA-fork/build -S/home/lwatan/data/SPH-EXA-fork -DSPH_EXA_WITH_GRACKLE=OFF
-cd build/sphexa 
+rm -rf build
+mkdir build-release
+
+cmake -B/home/lwatan/data/SPH-EXA-fork/build-release -S/home/lwatan/data/SPH-EXA-fork -DSPH_EXA_WITH_GRACKLE=OFF -DCMAKE_BUILD_TYPE=Release
+cd build-release/main/src/sphexa
 make sphexa
+cd /home/lwatan/data/SPH-EXA-fork
