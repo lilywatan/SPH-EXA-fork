48b9c85d9c2a7e79a72dd5add2440667a2f7d572
lily-uzh <lily.watanabe@uzh.ch>
2024-11-25 15:07:50 +0100

    edited run scripts

--- run_disk.sh
+++ run_disk.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=disk_comb_1mil         # Job name    (default: sbatch)
-#SBATCH --output=disk_comb_15.out        # Output file (default: slurm-%j.out)
-#SBATCH --error=disk_comb_15.err         # Error file  (default: slurm-%j.out)
+#SBATCH --job-name=disk_comb_20        # Job name    (default: sbatch)
+#SBATCH --output=disk_comb_20.out        # Output file (default: slurm-%j.out)
+#SBATCH --error=disk_comb_20_%j.err         # Error file  (default: slurm-%j.out)
 #SBATCH --cpus-per-task=1         # Number of CPUs per task
 #SBATCH --ntasks=1                # Number of tasks
 #SBATCH --ntasks-per-node=1      # Number of tasks per node§
 #SBATCH --mem-per-cpu=4G          # Memory per CPU
-#SBATCH --time=04:00:00           # Wall clock time limit
+#SBATCH --time=18:00:00           # Wall clock time limit
 
 module load openmpi
 module load hdf5
@@ -14,5 +14,5 @@ module load hdf5
 OMP_NUM_THREADS=8
 export OMP_NUM_THREADS
 
-srun build-release/main/src/sphexa/sphexa --init '/home/lwatan/data/disk5.hdf5' --prop std-angmom -s 15000 -w 10 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o '/home/lwatan/data/SPH-EXA-fork/output/run_disk_comb_15.hdf5'
+srun build-release/main/src/sphexa/sphexa --init '/home/lwatan/data/disk5.hdf5' --prop std-angmom -s 20000 -w 10 -f m,c,x,y,z,rho,p,vx,vy,vz,h -o '/home/lwatan/data/SPH-EXA-fork/output/run_disk_comb_20.hdf5'
 
