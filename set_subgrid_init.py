72a76a8d2b999daa0e91628b910938afcbe9a3d7
lily-uzh <lily.watanabe@uzh.ch>
2025-03-03 12:07:03 +0100

    debug init file changed to have 100k particles

--- set_subgrid_init.py
+++ set_subgrid_init.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 
 # File paths
 no_acc = '/home/lwatan/scratch/run_disk_cal_1e6_beta_planet_star0.hdf5'  # Calibration file
@@ -10,10 +11,10 @@ with h5py.File(input_subgrid, 'a') as sub, \
      h5py.File(no_acc, 'r') as cal, \
      h5py.File(beta, 'r') as beta_file:
     
-    # Get the "Step#25" dataset from the calibration file (was mistakenly Step#16)
+    # Get the "Step#15" dataset from the calibration file
     cal_obj = cal['Step#15']  # Make sure this is the correct step
 
-    # Get the "Step#0" dataset from the beta file
+    # Get the "Step#0" dataset from the beta file (has 100,000 particles)
     beta_obj = beta_file['Step#0']
 
     # Ensure that "Step#0" exists in the sub-grid file; create if not present
@@ -23,42 +24,41 @@ with h5py.File(input_subgrid, 'a') as sub, \
     # Get the "Step#0" dataset from the sub-grid file
     sub_obj = sub['Step#0']
 
-    ### Copy only those Attributes that exist in the beta file.
-    for attr in cal_obj.attrs.keys():
-        if attr in sub_obj.attrs:  # Fixed: Check in attrs, not sub_obj directly
-            del sub_obj.attrs[attr]  # Remove existing dataset if it exists
-        sub_obj.attrs[attr] = cal_obj.attrs[attr]
-
-    ### Copy Additional attributes from `beta` (if not present in `no_acc`)
-    for attr in beta_obj.attrs.keys():
-        if attr not in cal_obj.attrs:  # Only copy if it does not exist in `no_acc`
-            if attr in sub_obj.attrs:  # Fixed: Check in attrs
-                del sub_obj.attrs[attr]  # Remove existing dataset if it exists
-            sub_obj.attrs[attr] = beta_obj.attrs[attr]
-
-    ### Copy Datasets (Keys) from `no_acc` to `input_subgrid`
-    for key in cal_obj.keys():
-        if key in sub_obj.keys():  # Fixed: Check in keys()
-            del sub_obj[key]  # Remove existing dataset if it exists
-        cal_obj.copy(key, sub_obj, name=key)
-
-    ### Copy Additional Datasets (Keys) from `beta` (if not present in `no_acc`)
+    ### Copy All Datasets from `beta` to `input_subgrid`
     for key in beta_obj.keys():
-        if key not in cal_obj:  # Only copy if it does not exist in `no_acc`
-            if key in sub_obj.keys():  # Fixed: Check in keys()
-                del sub_obj[key]  # Remove existing dataset if it exists
-            beta_obj.copy(key, sub_obj, name=key)
+        if key in sub_obj.keys():  # Check if dataset exists
+            del sub_obj[key]  # Remove existing dataset if it exists
+        beta_obj.copy(key, sub_obj, name=key)
 
-    # Set iteration to 0
+    # Set iteration to 0 and other necessary attributes
     sub['Step#0'].attrs['iteration'] = 0
     sub['Step#0'].attrs['star::x'] = 0
     sub['Step#0'].attrs['star::y'] = 0
-    print(len(sub['Step#0']['x'][0:]))
-    sub['Step#0'].attrs['numParticlesGlobal'] = len(sub['Step#0']['x'][0:])
-    print(cal['Step#15'].attrs['numParticlesGlobal'])
-    print(sub['Step#0'].attrs['numParticlesGlobal'])
-    # sub['Step#0'].attrs['star::removal_limit_h'] 
-    del sub['Step#0'].attrs['star::inner_size'] 
 
+    # Get the current number of particles in Step#0
+    num_particles_beta = len(sub['Step#0']['x'][0:])
+    target_num_particles = 10000
 
-    print("All attributes and datasets copied successfully from 'Step#25' to 'Step#0'.")
+    # Print current number of particles
+    print(f"Current number of particles (from beta): {num_particles_beta}")
+
+    # Now, we will update the first 99935 particles with data from `cal`
+    num_particles_cal = len(cal_obj['x'])
+
+    # If the number of particles in `cal` is less than 99935, we adjust this by filling dummy particles
+    if num_particles_cal < 99935:
+        print(f"Warning: The calibration file has fewer particles than expected ({num_particles_cal} < 99935).")
+        # You can decide here how to handle this case (e.g., pad with dummy particles).
+    else:
+        # Update the first 99935 particles from `cal` to match the data in the `cal` file
+        for key in cal_obj.keys():
+            # Ensure the data in `cal` exists and update the first 99935 particles
+            if key in sub_obj:
+                sub_obj[key][:99935] = cal_obj[key][:99935]  # Update the first 99935 particles
+
+    # After updating, ensure `numParticlesGlobal` is correctly set
+    sub['Step#0'].attrs['numParticlesGlobal'] = 100000
+
+    # Print final number of particles
+    print(f"Final number of particles in Step#0: {len(sub['Step#0']['x'][0:])}")
+    print("All attributes and datasets copied successfully from 'Step#0' of beta and updated with 'cal'.")
